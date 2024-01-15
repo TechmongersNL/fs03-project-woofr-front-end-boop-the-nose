594f4bb31799abf532d649feab3f13e1badf27a7
isvandegraaff <i.s.vandegraaff@gmail.com>
2024-01-15 22:41:11 +0100

    Changed homepage - still need to add navigation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import LoginForm from "./pages/loginPage";
 import "./Styles/App.css";
 import "./Styles/Feedpost.css";
+import "./Styles/HomePage.css";
+import "./Styles/loginPage.css";
 import { Routes, Route } from "react-router-dom";
-
 
 import HomePage from "./Pages/Homepage";
 import FeedPage from "./Pages/FeedPage";
 import SignUp from "./Pages/SignUp";
+import LoginForm from "./Pages/loginPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/login" element={<LoginForm />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/feed" element={<FeedPage />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LoginForm />} />
       </Routes>
     </div>
   );
